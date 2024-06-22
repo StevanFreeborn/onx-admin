9bd844be675c48e976ad805028ad9b58f347843c
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-06-21 23:13:01 -0500

    fix: remove unnecessary checks

--- src/OnxAdmin.AnthropicConnector/Models/Content.cs
+++ src/OnxAdmin.AnthropicConnector/Models/Content.cs
@@ -5,7 +5,7 @@ namespace OnxAdmin.AnthropicConnector.Models;
 public abstract class Content
 {
   public string Type { get; init; } = string.Empty;
-  
+
   [JsonConstructor]
   internal Content()
   {
@@ -13,13 +13,6 @@ public abstract class Content
 
   protected Content(string type)
   {
-    ArgumentNullException.ThrowIfNull(type, nameof(type));
-    
-    if (ContentType.IsValidContentType(type) is false)
-    {
-      throw new ArgumentException($"Invalid content type: {type}");
-    }
-    
     Type = type;
   }
 }
