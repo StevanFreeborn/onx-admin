797ebdf29840cb600bf916bf030b1635938497fd
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-07-10 22:09:00 -0500

    chore: change client resource name

--- src/OnxAdmin.AppHost/Program.cs
+++ src/OnxAdmin.AppHost/Program.cs
@@ -3,7 +3,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 var api = builder.AddProject<Projects.OnxAdmin_API>("api")
   .WithExternalHttpEndpoints();
 
-builder.AddNpmApp("vue", "../OnxAdmin.Client")
+builder.AddNpmApp("client", "../OnxAdmin.Client")
   .WithReference(api)
   .WithHttpsEndpoint(port: 4000, env: "PORT")
   .WithExternalHttpEndpoints()
