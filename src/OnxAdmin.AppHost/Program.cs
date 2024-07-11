9fdf4de130ffd9794051dddff715b5545e396c17
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-07-10 22:04:30 -0500

    chore: use use distributed architecture with aspire

---
+++ src/OnxAdmin.AppHost/Program.cs
@@ -0,0 +1,12 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var api = builder.AddProject<Projects.OnxAdmin_API>("api")
+  .WithExternalHttpEndpoints();
+
+builder.AddNpmApp("vue", "../OnxAdmin.Client")
+  .WithReference(api)
+  .WithHttpsEndpoint(port: 4000, env: "PORT")
+  .WithExternalHttpEndpoints()
+  .PublishAsDockerFile();
+
+builder.Build().Run();
